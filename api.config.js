58cfbbbc152b66f2a85e48e504e77f80acdda0da
Ryan Lee <rlee603166@gmail.com>
2025-05-12 15:16:01 -0700

    update for ssl

--- api.config.js
+++ api.config.js
@@ -1,6 +1,6 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_ASTRALIS_API_URL;
- 
+
 export const ENDPOINTS = {
-    users: "api/search",
+    users: `${API_BASE_URL}/users`,
     search: `${API_BASE_URL}/search`,
 };
